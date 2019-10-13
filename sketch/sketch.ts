2889d42cf63b34e7714d48330612b516ea292ddb
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2019-10-13 22:25:03 +1000

    bare minimal sketch

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,15 +1,20 @@
-let morph: Morph;
-function setup() {
-    createCanvas(windowWidth, windowHeight)
-    morph = new Morph();
-    morph.setup();
+const sketch = (p : p5) =>  {
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight)
+  }
+
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight)
+  }
+
+  p.draw = () => {
+    p.background(0)
+
+    p.fill(200)
+    p.stroke(255)
+    p.strokeWeight(5)
+    p.rect(100, 100, p.width-200, p.height-200)
+  }
 }
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-    background(100);
-    morph.draw();
-}
+new p5(sketch)
