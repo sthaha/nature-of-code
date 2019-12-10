74318a703654d2aa9585af81cf194e7afef5a150
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2019-12-10 23:27:59 +1000

    rotation: of a single rect

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,19 +1,27 @@
+
 const sketch = (p : p5) =>  {
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight)
+    p.angleMode(p.RADIANS)
   }
 
-  p.windowResized = () => {
-    p.resizeCanvas(p.windowWidth, p.windowHeight)
-  }
+  p.windowResized = () => { p.resizeCanvas(p.windowWidth, p.windowHeight) }
+
+  let angle = 0
 
   p.draw = () => {
     p.background(0)
 
-    p.fill(200)
-    p.stroke(255)
-    p.strokeWeight(5)
-    p.rect(100, 100, p.width-200, p.height-200)
+    const w = 100
+    const h = 50
+    const x = -w/2
+    const y = -h/2
+    p.translate(p.width/2, p.height/2)
+    p.rotate(angle)
+    p.rect(x, y, w, h)
+
+    const d = p.map(p.mouseX, 0, p.width, -p.PI/4, p.PI/4)
+    angle += d
   }
 }
 
