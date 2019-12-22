b26d22ca76afacd32929f0675650ec366418ca1b
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2019-12-22 15:53:17 +1000

    polar cords spiral

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,19 +1,41 @@
 const sketch = (p : p5) =>  {
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight)
+    p.background(0)
+    p.noLoop()
   }
 
   p.windowResized = () => {
     p.resizeCanvas(p.windowWidth, p.windowHeight)
   }
 
+
+  let angle : number = 0
+  let len : number = 1
   p.draw = () => {
-    p.background(0)
+    p.translate(p.width/2, p.height/2)
+
 
     p.fill(200)
     p.stroke(255)
     p.strokeWeight(5)
-    p.rect(100, 100, p.width-200, p.height-200)
+
+    const rad = angle * 2 * p.PI / 360
+    const x = len * p.cos(angle)
+    const y = len * p.sin(angle)
+    p.point(x, y)
+
+    const fr = p.max(15, p.frameRate())
+    angle += 1.0/ fr
+    len += 2.0/ fr
+  }
+
+  p.keyPressed = () => {
+    switch(p.keyCode) {
+      case 32: p.redraw()
+      case p.ESCAPE: p.redraw()
+      case p.RETURN: p.loop()
+    }
   }
 }
 
