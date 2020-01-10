80bbb8b5a534d93b2af83ca7d2ba114d468440dd
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2020-01-10 22:58:59 +1000

    Target can be dragged by applying mixin

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,3 +1,77 @@
+function applyMixins(derivedCtor: any, baseCtors: any[]) {
+    baseCtors.forEach(baseCtor => {
+        Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+            Object.defineProperty(derivedCtor.prototype, name, Object.getOwnPropertyDescriptor(baseCtor.prototype, name));
+        });
+    });
+}
+
+class P5MouseEvent {
+  clientX: number
+  clientY: number
+}
+
+class Target {
+  p: p5
+
+  pos: p5.Vector
+  radius = 20
+  dragged = false
+
+  constructor(p :p5, pos: p5.Vector) {
+    this.p = p
+    this.pos = pos
+  }
+
+  draw() {
+    const {p, pos, radius} = this
+
+    p.strokeWeight(3)
+    p.stroke(128, 200, 200, 180)
+    p.fill(128, 200, 200, 180)
+
+    const {x, y} = pos
+    const  d = radius * 2
+    p.ellipse(x, y, d, d)
+  }
+
+  hasPoint(x: number, y: number) {
+    const {p, pos, radius} = this
+    return pos.dist(p.createVector(x, y)) <= radius
+  }
+}
+
+
+class Draggable  {
+  p: p5
+  pos: p5.Vector
+  dragged = false
+
+  onDragStarted(e: P5MouseEvent) {
+    this.dragged = true
+    console.log("drag start")
+  }
+
+  onDragStopped(e: P5MouseEvent) {
+    this.dragged = false
+    console.log("drag stopped")
+  }
+
+  onMouseDragged(e: P5MouseEvent) {
+    if (!this.dragged) {
+      return
+    }
+
+    console.log("dragging ...")
+
+    this.pos = this.p.createVector(e.clientX, e.clientY)
+  }
+}
+
+interface Target extends Draggable {}
+
+applyMixins(Target, [Draggable]);
+
 const sketch = (p : p5) =>  {
 
   let isLooping = true
@@ -22,18 +96,39 @@ const sketch = (p : p5) =>  {
     p.resizeCanvas(p.windowWidth, p.windowHeight)
   }
 
+  const v = (x: number, y: number) => p.createVector(x, y)
+  let target = new Target(p, v(300, 300 ))
+
+
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight)
-    noLoop()
+    //noLoop()
   }
 
   p.draw = () => {
     p.background(0)
+    target.draw()
+  }
 
-    p.fill(200)
-    p.stroke(255)
-    p.strokeWeight(5)
-    p.rect(100, 100, p.width-200, p.height-200)
+
+  p.mousePressed = (e: P5MouseEvent) => {
+    if (!target.hasPoint(e.clientX, e.clientY)) {
+      console.log("target clicked outside", e)
+      return
+    }
+    target.onDragStarted(e)
+  }
+
+  p.mouseReleased = (e: P5MouseEvent) => {
+    if (!target.hasPoint(e.clientX, e.clientY)) {
+      console.log("target clicked outside", e)
+      return
+    }
+    target.onDragStopped(e)
+  }
+
+  p.mouseDragged = (e: P5MouseEvent) => {
+    target.onMouseDragged(e)
   }
 }
 
