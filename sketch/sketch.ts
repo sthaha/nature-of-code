22fb438e5a49c0dfb6c25e1f7c2d9bd1673151d6
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2020-02-09 00:38:32 +1000

    Draws path and sprite

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,3 +1,91 @@
+class Path {
+  p: p5
+  points: p5.Vector[]
+  radius: number
+
+  constructor(p: p5, points : p5.Vector[]) {
+    this.p = p
+    this.points = [...points]
+
+    this.radius = 40
+  }
+
+  draw() {
+    const {p, points, radius} = this
+
+
+    for (let i =0; i< points.length-1; i++){
+      const p1 = points[i]
+      const p2 = points[i+1]
+
+      p.strokeWeight(radius)
+      p.stroke(250, 120)
+      p.line(p1.x, p1.y, p2.x, p2.y)
+
+      p.strokeWeight(3)
+      p.stroke("orange")
+      p.line(p1.x, p1.y, p2.x, p2.y)
+
+      //const m = (p2.y - p1.y)/(p2.x - p1.x)
+      //const c = p2.y - (m * p2.x)
+
+      //const y = (x: number, r: number) => m*x + c + r
+
+      //p.stroke("purple")
+      //p.line(p1.x, y(p1.x, radius), p2.x, y(p2.x, radius))
+
+      //p.stroke("green")
+      //p.line(p1.x, y(p1.x, -radius), p2.x, y(p2.x, -radius))
+
+    }
+  }
+}
+
+
+class Sprite {
+  p: p5
+  pos: p5.Vector
+  v: p5.Vector
+  radius: number
+
+  constructor(p: p5, pos: p5.Vector, v: p5.Vector) {
+    this.p = p
+    this.pos = pos
+    this.v = v
+    this.radius = 10
+  }
+
+  draw() {
+    const {p, radius, pos, v}= this
+
+    p.stroke(100,250, 200, 180)
+    p.strokeWeight(8)
+    p.point(pos.x, pos.y)
+
+    p.stroke("gray")
+    p.strokeWeight(2)
+    p.line(pos.x, pos.y, pos.x + v.x, pos.y + v.y)
+
+    p.push()
+      p.translate(pos.x, pos.y)
+      p.rotate(v.heading() - p.PI/2)
+
+
+      p.stroke(100, 200, 125, 150)
+      p.strokeWeight(4)
+      p.fill(100, 200, 125, 110)
+      p.beginShape()
+        p.vertex(radius/2, 0)
+        p.vertex(0, 15)
+        p.vertex(-radius/2, 0)
+      p.endShape()
+    p.pop()
+
+    console.log("heading", v.heading())
+  }
+}
+
+
 const sketch = (p : p5) =>  {
 
   let isLooping = true
@@ -22,6 +110,26 @@ const sketch = (p : p5) =>  {
     p.resizeCanvas(p.windowWidth, p.windowHeight)
   }
 
+  const v = (x: number, y: number) => p.createVector(x, y)
+
+  const pts = [
+    v(0, 500),
+    v(50, 400),
+    v(200, 600),
+    v(250, 500),
+    v(300, 500),
+    v(400, 400),
+  ]
+
+  const path = new Path(p, pts)
+  const sprites = [
+    new Sprite(p, v(250, 200), v(20, 0)),
+    new Sprite(p, v(200, 200), v(0, 20)),
+    //new Sprite(p, v(200, 200), v(20, 20)),
+    //new Sprite(p, v(200, 200), v(20, -20)),
+    //new Sprite(p, v(200, 200), v(-20, 0)),
+  ]
+
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight)
     noLoop()
@@ -33,7 +141,10 @@ const sketch = (p : p5) =>  {
     p.fill(200)
     p.stroke(255)
     p.strokeWeight(5)
-    p.rect(100, 100, p.width-200, p.height-200)
+    path.draw()
+    for (const s of sprites) {
+      s.draw()
+    }
   }
 }
 
