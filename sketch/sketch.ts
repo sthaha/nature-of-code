326aafe65586a9aaf1e849856fb5b4f4a19235b1
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2020-04-19 23:55:36 +1000

    Lsystem graphics

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,3 +1,64 @@
+
+class LSystem {
+  p: p5
+  axiom: string
+  ruleset: {[id: string]: string}
+  constructor(p: p5, axiom: string, ruleset: {[id: string]: string}) {
+    this.p = p
+    this.axiom = axiom
+    this.ruleset = ruleset
+  }
+
+  compute() {
+    const {axiom, ruleset} = this
+    this.axiom = axiom.split("").map((x:string) => ruleset[x] || x).join("")
+  }
+}
+
+class AxiomInterpreter {
+  p: p5
+  lsystem: LSystem
+  len: number = 100
+  gen: number = 0
+  constructor(p: p5, lsystem: LSystem) {
+    this.p = p
+    this.lsystem = lsystem
+    this.gen = 0
+  }
+
+  interpret() {
+    const {p, lsystem, gen, len} = this
+    const {axiom} = lsystem
+    const angle = p.radians(25)
+
+    const actions: {[id:string]: ()=>any } = {
+      F: () => {p.line(0, 0, 0, -len); p.translate(0, -len)},
+      '-': () => p.rotate(-angle),
+      '+': () => p.rotate(angle),
+      '[': () => p.push(),
+      ']': () => p.pop(),
+    }
+
+    console.group()
+    //console.log("axiom: ", axiom)
+    p.strokeWeight(p.map(gen, 0, 5, 3, 1))
+    p.stroke(255, p.map(gen, 0, 5, 100, 60))
+
+    p.translate(p.width/2, p.height)
+    p.push()
+      for (let i = 0; i<axiom.length; i++) {
+        const ch = axiom.charAt(i)
+        const action = actions[ch]
+        //console.log("ch:", ch)
+        action && action()
+      }
+    p.pop()
+    console.groupEnd()
+    this.len *= 0.5
+    this.gen++
+  }
+}
+
 const sketch = (p : p5) =>  {
 
   let isLooping = true
@@ -17,18 +78,24 @@ const sketch = (p : p5) =>  {
     p.resizeCanvas(p.windowWidth, p.windowHeight)
   }
 
+  const lsys = new LSystem(p, "F", {
+    F: "FF+[+F-F-F]-[-F+F+F]",
+  })
+
+  const interpreter = new AxiomInterpreter(p, lsys)
+
   p.setup = () => {
+    p.noCanvas()
     p.createCanvas(p.windowWidth, p.windowHeight)
+    p.background(0)
     noLoop()
+
+
   }
 
   p.draw = () => {
-    p.background(0)
-
-    p.fill(200)
-    p.stroke(255)
-    p.strokeWeight(5)
-    p.rect(100, 100, p.width-200, p.height-200)
+    lsys.compute()
+    interpreter.interpret()
   }
 }
 
